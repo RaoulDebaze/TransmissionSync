e82e7e544123260cdf868ddaa4cec5615e64c457
Raoul Debaze <raoul.debaze@laposte.net>
2013-08-20 13:58:07 +0200

    Init with transmission-trunk-r14169 tarball

---
+++ libtransmission/version.h
@@ -0,0 +1,10 @@
+#define PEERID_PREFIX             "-TR282Z-"
+#define USERAGENT_PREFIX          "2.82+"
+#define SVN_REVISION              ""
+#define SVN_REVISION_NUM          
+#define SHORT_VERSION_STRING      "2.82+"
+#define LONG_VERSION_STRING       "2.82+ ()"
+#define VERSION_STRING_INFOPLIST  2.82+
+#define MAJOR_VERSION             2
+#define MINOR_VERSION             82
+#define TR_NIGHTLY_RELEASE        1
