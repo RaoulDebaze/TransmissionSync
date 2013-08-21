d317dc848d8953fe87208f31245735b9f3d7aad1
Raoul Debaze <raoul.debaze@laposte.net>
2013-08-21 22:59:59 +0200

    Try to add Hellow world on Mac OS

---
+++ sibsync/sibsync.c
@@ -0,0 +1,20 @@
+/*
+ * This file Copyright (C) Mnemosyne LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * $Id: daemon.c 14084 2013-06-07 23:31:26Z jordan $
+ */
+
+#include <errno.h>
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* exit, atoi */
+
+main()
+{
+    printf("Hello World");
+}
